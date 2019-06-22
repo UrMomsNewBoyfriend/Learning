7bde519f8e02ddf8d065648c33d4d1fdc17dd13e
theRealTinyRick <31013885+theRealTinyRick@users.noreply.github.com>
2019-06-22 10:22:04 -0400

    Initial Checkin

---
+++ TopDownCharacter/Assets/TopDownAssets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 cameraMvmt;
+
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    public void CameraPosition(Vector3 playerPosition)
+    {
+        transform.position = playerPosition + cameraOffset;
+        transform.LookAt(playerPosition);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ TopDownCharacter/Assets/TopDownAssets/TopDownController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    Transform myTransform;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private float turnSpeed;
+
+    [SerializeField]
+    private GameObject camera;
+
+    private CameraMovement cameraMvmt;
+
+    private Animator animator;
+
+    private void Start()
+    {
+
+        animator = GetComponentInChildren<Animator>();
+        if (camera != null)
+        {
+            cameraMvmt = camera.GetComponent<CameraMovement>();
+        }
+        myTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 movementDir = new Vector3(Horizontal, 0, Vertical);
+        movementDir = camera.transform.TransformDirection(movementDir);
+        movementDir.y = 0;
+        transform.position += movementDir;
+
+        if (Horizontal != 0 || Vertical != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementDir);
+            transform.rotation = targetRotation;
+
+            animator.SetBool("isMoving", true);
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+
+        }
+    }
+
+    void LateUpdate()
+    {
+       if (cameraMvmt != null)
+        {
+            cameraMvmt.CameraPosition(myTransform.position);
+        }
+    }
+
+    
+}
